0c2cfa13786c6f68777268a2c7254336e84bfd5d
kvl <kvl_93@outlook.com>
2023-04-07 17:53:59 +0300

    feat(webextension): added interop with javascript api through wasm_bindgen

--- browser/keycrab_ext/release.sh
+++ browser/keycrab_ext/release.sh
@@ -4,10 +4,12 @@ rm -r ../ff/assets || true
 rm ../ff/index.html || true
 rm ../ff/index.js || true
 rm ../ff/tailwind.css || true
+rm ../ff/sample.js || true
 
 dioxus build --release \
 && rm dist/index.html \
 && cp index.html ../ff \
 && cp -r dist/assets ../ff \
 && cp dist/index.js ../ff \
-&& cp dist/tailwind.css ../ff
+&& cp dist/tailwind.css ../ff \
+&& cp dist/sample.js ../ff
--- browser/keycrab_ext/src/main.rs
+++ browser/keycrab_ext/src/main.rs
@@ -1,9 +1,73 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
+use js_sys::Promise;
+use serde::{Deserialize, Serialize};
+use serde_wasm_bindgen::{from_value, to_value};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
+
+#[derive(Serialize, Deserialize)]
+struct TabQueryArgs {
+    active: bool,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Tab {
+    id: Option<i32>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ExecuteScriptArgs {
+    target: InjectionTarget,
+    files: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct InjectionTarget {
+    #[serde(rename = "tabId")]
+    tab_id: i32,
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    #[wasm_bindgen(js_namespace = document, js_name = createTextNode)]
+    fn create_text_node(s: &str);
+
+    #[wasm_bindgen(catch, js_namespace = ["browser", "tabs"])]
+    fn query(s: JsValue) -> Result<JsValue, JsValue>;
+
+    #[wasm_bindgen(catch, js_namespace = ["chrome", "scripting"])]
+    fn executeScript(s: JsValue) -> Result<(), JsValue>;
+}
 
 fn main() {
     dioxus_web::launch(App);
+}
+
+async fn load_script() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once();
+
+    let tab_query_args = TabQueryArgs { active: true };
+
+    let tab_query: Promise = query(to_value(&tab_query_args)?)?.into();
+    let tab_query = JsFuture::from(tab_query).await?;
+    let tabs: Vec<Tab> = from_value(tab_query)?;
+
+    for tab in tabs {
+        let Some(tab_id) = tab.id else { continue; };
+        let args = ExecuteScriptArgs {
+            target: InjectionTarget { tab_id },
+            files: vec!["sample.js".to_string()],
+        };
+        let value = serde_wasm_bindgen::to_value(&args).unwrap();
+        executeScript(value)?;
+    }
+
+    Ok(())
 }
 
 fn Counter(cx: Scope) -> Element {
@@ -19,6 +83,12 @@ fn Counter(cx: Scope) -> Element {
 }
 
 fn App(cx: Scope) -> Element {
+    let future = use_future(cx, (), |_| async move {
+        if let Err(e) = load_script().await {
+            log(&format!("{e:?}"));
+        }
+    });
+    let _ = future.value();
     cx.render(rsx! {
         Counter{},
     })
