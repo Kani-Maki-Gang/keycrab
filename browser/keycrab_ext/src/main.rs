ae2d4f5bd27f92ab82afb269e8016efb4a7b35e9
AlkisChristidis <p_christidis@outlook.com>
2023-04-09 23:26:10 +0300

    feat(webextension): added injected script, message passing and request to the backend

--- browser/keycrab_ext/src/main.rs
+++ browser/keycrab_ext/src/main.rs
@@ -2,10 +2,31 @@
 
 use dioxus::prelude::*;
 use js_sys::Promise;
+use anyhow::Result;
 use serde::{Deserialize, Serialize};
 use serde_wasm_bindgen::{from_value, to_value};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::JsFuture;
+
+#[derive(Serialize, Deserialize)]
+struct Credential {
+    id: i32,
+    domain: String,
+    username: String,
+    password: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct SearchResponse {
+    credentials: Vec<Credential>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct  SendMessageArgs {
+    command: String,
+    username: String,
+    password: String,
+}
 
 #[derive(Serialize, Deserialize)]
 struct TabQueryArgs {
@@ -15,6 +36,7 @@ struct TabQueryArgs {
 #[derive(Serialize, Deserialize)]
 struct Tab {
     id: Option<i32>,
+    url: Option<String>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -38,6 +60,9 @@ extern "C" {
     fn create_text_node(s: &str);
 
     #[wasm_bindgen(catch, js_namespace = ["browser", "tabs"])]
+    fn sendMessage(tabId: i32, args: JsValue) -> Result<JsValue, JsValue>;
+
+    #[wasm_bindgen(catch, js_namespace = ["browser", "tabs"])]
     fn query(s: JsValue) -> Result<JsValue, JsValue>;
 
     #[wasm_bindgen(catch, js_namespace = ["chrome", "scripting"])]
@@ -46,6 +71,14 @@ extern "C" {
 
 fn main() {
     dioxus_web::launch(App);
+}
+
+async fn find_active_tab() -> Result<Tab, JsValue> {
+    let tab_query_args = TabQueryArgs { active: true };
+    let tab_query: Promise = query(to_value(&tab_query_args)?)?.into();
+    let tab_query = JsFuture::from(tab_query).await?;
+    let tabs: Vec<Tab> = from_value(tab_query)?;
+    tabs.into_iter().next().ok_or(JsValue::from_str("No active tab"))
 }
 
 async fn load_script() -> Result<(), JsValue> {
@@ -61,7 +94,7 @@ async fn load_script() -> Result<(), JsValue> {
         let Some(tab_id) = tab.id else { continue; };
         let args = ExecuteScriptArgs {
             target: InjectionTarget { tab_id },
-            files: vec!["sample.js".to_string()],
+            files: vec!["fill_form.js".to_string()],
         };
         let value = serde_wasm_bindgen::to_value(&args).unwrap();
         executeScript(value)?;
@@ -70,13 +103,64 @@ async fn load_script() -> Result<(), JsValue> {
     Ok(())
 }
 
+async fn send_command(command: String, username: String, password: String) -> Result<(), JsValue> {
+    let tab = find_active_tab().await?;
+    let send_message_args = SendMessageArgs {
+        command,
+        username,
+        password,
+    };
+    let send_msg: Promise = sendMessage(
+        tab.id.unwrap(),
+        to_value(&send_message_args)?
+        )?.into();
+
+    JsFuture::from(send_msg).await?;
+    Ok(())
+}
+
+async fn get_login_for_url(url: String) -> Result<SearchResponse> {
+    let client = reqwest::Client::new();
+    let response = client.get("http://localhost:8000/domain/search")
+        .query(&[("q", url)])
+        .send()
+        .await?
+        .json()
+        .await?;
+    Ok(response)
+}
+
+async fn get_login_for_current_tab() -> Result<()> {
+    let tab = find_active_tab().await.map_err(|_| anyhow::anyhow!("No active tab"))?;
+    let url = tab.url.unwrap();
+    let credentials = get_login_for_url(url)
+        .await?
+        .credentials
+        .into_iter()
+        .next()
+        .ok_or(anyhow::anyhow!("No credentials found"))?;
+
+    send_command(
+        credentials.username,
+        credentials.password,
+        "fill".to_string()
+        ).await.map_err(|_| anyhow::anyhow!("Failed to send command"))
+}
+
 fn Counter(cx: Scope) -> Element {
     let count = use_state(&cx, || 0);
 
     cx.render(rsx! {
         button {
             class: "bg-current",
-            onclick: move |_| count.set(count + 1),
+            onclick: move |_|  {
+                let _ = use_future(cx, (), |_| async move {
+                    if let Err(e) = get_login_for_current_tab().await {
+                        log(&format!("{e:?}"));
+                    }
+                }).value();
+                count.set(count + 1);
+            },
             "Reset {count}"
         }
     })
