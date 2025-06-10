45a6f6d284e6085485ffa3af28dae145019f5ca1
Kostas Vlachos <kvl_93@outlook.com>
2025-06-11 00:10:08 +0300

    fix: Fixed some issues with the title bar and its padding

--- browser/keycrab_ext/src/app.rs
+++ browser/keycrab_ext/src/app.rs
@@ -5,7 +5,7 @@ use leptos::prelude::*;
 pub fn App() -> impl IntoView {
     view! {
         <div class="flex flex-col gap-4">
-            <div class="fixed top-0 right-0 left-0 border-b-1 border-slate-600 px-6 z-40">
+            <div class="fixed top-0 right-0 left-0 border-b-1 border-slate-600 z-40">
                 <Title />
             </div>
             <div class="pt-24">
--- browser/keycrab_ext/src/title.rs
+++ browser/keycrab_ext/src/title.rs
@@ -4,13 +4,15 @@ use leptos::prelude::*;
 #[component]
 pub fn Title() -> impl IntoView {
     view! {
-        <div class="flex flex-row items-center gap-4 h-24 w-full bg-slate-800">
-            <img class="size-12 z-40" src="./icons/keycrab-logo-ext.png" />
-            <div class="grow text-3xl">
-                <span class="text-gray-500">"Key"</span>
-                <span class="text-white">"crab"</span>
+        <div class="grid items-center h-24 bg-slate-800">
+            <div class="flex flex-row items-center gap-4 px-6">
+                <img class="size-12 z-40" src="./icons/keycrab-logo-ext.png" />
+                <div class="grow text-3xl">
+                    <span class="text-gray-500">"Key"</span>
+                    <span class="text-white">"crab"</span>
+                </div>
+                <IconButton icon="iconoir-settings" />
             </div>
-            <IconButton icon="iconoir-settings" />
         </div>
     }
 }
